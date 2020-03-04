64c5583e0dc029dd8336d38de43fe03a75d413a3
Grace Winters <53794604+gwinters02@users.noreply.github.com>
2020-03-04 10:27:58 -0600

    updates

---
+++ index.php
@@ -0,0 +1,41 @@
+<php include 'includes/header.php'; ?>
+
+    <!--Start Landing Page Section-->
+<div class="landing">
+  <div class="home-wrap">
+    <div class="home-inner"></div>
+  </div>
+</div>
+
+<!--Start of Carousel-->
+<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <div class="row dark">
+      <img src="../Petalz//Pictures/welcome.jpg" width = "250" height="725" class="d-block w-100" alt="...">
+        <div class="caption text-center">
+        <h1>Welcome to Petalz</h1>
+        <a class="btn btn-outline-light btn-lg" href="#course">Get Started</a>
+        </div>
+      </div>
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<!--End of Carousel-->
+
+  </body>
+</html>
